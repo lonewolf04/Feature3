0ec29b1cf7d27be5c22aae8037d39b19ceeef759
shobhana <shobhana224@gmail.com>
2016-04-10 18:07:50 +0530

    Initial commit

---
+++ app/src/main/java/com/example/shobhana/feature3/Gps.java
@@ -0,0 +1,105 @@
+package com.example.shobhana.feature3;
+
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+
+/**
+ * Created by shobhana on 10/04/16.
+ */
+public class Gps {
+
+    private LocationManager mlocation;
+    private LocationListener nlocation;
+    private Location llocation;
+
+    String provider = "";
+    Context context;
+    double lat,lng;
+    String latlng,lastLocation;
+
+    public Gps(Context context) {
+
+        this.context = context;
+    }
+
+
+    public void getLocation() {
+
+
+        mlocation = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+
+        Criteria criteria = new Criteria();
+        provider = mlocation.getBestProvider(criteria, false);
+        llocation = mlocation.getLastKnownLocation(provider);
+
+        System.out.println("location"+llocation);
+        lastLocation=llocation.getLatitude()+","+llocation.getLongitude();
+
+        nlocation = new gpsLocation();
+
+        if (ActivityCompat.checkSelfPermission(context, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(context, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mlocation.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 10, nlocation);
+
+
+    }
+
+    public String getCoordinates(){
+
+        if(latlng!=null)
+            return latlng;
+
+        else
+            return lastLocation;
+    }
+
+    private class gpsLocation implements LocationListener{
+
+        //all overriden method definitions are auto generated
+
+        String coordinates;
+
+        @Override
+        public void onLocationChanged(Location location) {
+
+            lat=location.getLatitude();
+            lng=location.getLongitude();
+
+            latlng=lat+","+lng;
+            System.out.println("latlng"+latlng);
+
+
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
+
+
+    }
+}
---
+++ app/src/main/java/com/example/shobhana/feature3/MainActivity.java
@@ -0,0 +1,395 @@
+package com.example.shobhana.feature3;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.LocalBroadcastManager;
+import android.support.v7.app.AppCompatActivity;
+import android.telephony.TelephonyManager;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity  {
+
+    private static final String TAG = "MainActivity";
+    final int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
+
+    EditText text;
+    String textMsg;
+    String mPhoneNumber="9844116260";
+
+    private BroadcastReceiver mRegistrationBroadcastReceiver;
+    private ProgressBar mRegistrationProgressBar;
+    private TextView mInformationTextView;
+    private boolean isReceiverRegistered;
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        /*Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);*/
+
+        findViewById(R.id.button).setOnClickListener(handleClick);
+        findViewById(R.id.lbutton).setOnClickListener(handleClick1);
+
+        //Gps gpsObject=new Gps(this);
+
+
+        text = (EditText) findViewById(R.id.text);
+
+        mRegistrationBroadcastReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+
+                SharedPreferences sharedPreferences =
+                        PreferenceManager.getDefaultSharedPreferences(context);
+                boolean sentToken = sharedPreferences
+                        .getBoolean(TokenStatus.SENT_TOKEN_TO_SERVER, false);
+                if (sentToken) {
+                    //mInformationTextView.setText(getString(R.string.gcm_send_message));
+                    Toast.makeText(getBaseContext(), "token obtained", Toast.LENGTH_LONG).show();
+                } else {
+                    //mInformationTextView.setText(getString(R.string.token_error_message));
+                    Toast.makeText(getBaseContext(), "Error...", Toast.LENGTH_LONG).show();
+                }
+            }
+
+
+        };
+
+        //mInformationTextView = (TextView) findViewById(R.id.informationTextView);
+
+        registerReceiver();
+
+
+        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });*/
+
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+        if (checkPlayServices()) {
+            System.out.println("services are enabled");
+            Intent intent = new Intent(this, RegistrationIntentService.class);
+            startService(intent);
+            //sendMessage();
+        } else {
+            //go to message interface
+            Log.i("Token", "Token already obtained");
+            //sendMessage();
+
+
+        }
+
+
+    }
+
+    private View.OnClickListener handleClick = new View.OnClickListener() {
+        public void onClick(View arg0) {
+            Button btn = (Button) arg0;
+            textMsg = MainActivity.this.text.getText().toString();
+
+            sendMessage();
+
+
+        }
+    };
+
+    private View.OnClickListener handleClick1 = new View.OnClickListener() {
+        public void onClick(View arg0) {
+            Button btn = (Button) arg0;
+
+            String coordinates;
+
+            Gps gpsObject=new Gps(getApplicationContext());
+            gpsObject.getLocation();
+            coordinates=gpsObject.getCoordinates();
+            sendLatLng(coordinates);
+
+        }
+    };
+
+
+    private void sendLatLng(String coordinates){
+
+        String message="message="+coordinates+"&phonenumber="+mPhoneNumber;
+
+        System.out.println(coordinates);//null value
+        SendCoordinates object=new SendCoordinates(message);
+        object.init();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        registerReceiver();
+    }
+
+    @Override
+    protected void onPause() {
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(mRegistrationBroadcastReceiver);
+        isReceiverRegistered = false;
+        super.onPause();
+    }
+
+    private void registerReceiver(){
+        if(!isReceiverRegistered) {
+            LocalBroadcastManager.getInstance(this).registerReceiver(mRegistrationBroadcastReceiver,
+                    new IntentFilter(TokenStatus.REGISTRATION_COMPLETE));
+            isReceiverRegistered = true;
+        }
+    }
+
+
+
+    private void sendMessage() {
+
+        //String s="Hello from gcm";
+        String message=null;
+
+        message="message="+textMsg+"&phonenumber="+mPhoneNumber;
+        new SendMessage().execute(message);
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+    public boolean checkPlayServices() {
+        GoogleApiAvailability googleApi = GoogleApiAvailability.getInstance();
+        int result = googleApi.isGooglePlayServicesAvailable(this);
+        System.out.println(result);
+        if (result != ConnectionResult.SUCCESS) {
+            if (googleApi.isUserResolvableError(result)) {
+                googleApi.getErrorDialog(this, result, PLAY_SERVICES_RESOLUTION_REQUEST)
+                        .show();
+            } else {
+                Log.i(TAG, "This device is not supported.");
+                finish();
+            }
+
+            return false;
+        }
+
+        else
+        {
+            System.out.println("enabled");
+            return true;
+        }
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.example.shobhana.feature3/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.example.shobhana.feature3/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
+    }
+
+
+
+    class SendMessage extends AsyncTask<String,String,String> {
+
+        //This is the thread which handles data transfer:Required so that UI will not freeze
+
+
+        // private static final String TAG = " ";
+
+        /** Code performing Http connection and POST request to server
+         * is implemented here */
+        @Override
+        protected String doInBackground(String... params)
+        {
+            String JsonResponse = null;
+            String PayloadData=params[0];
+
+            HttpURLConnection connection = null;
+            BufferedReader reader = null;
+            try
+            {
+                URL url = new URL("http://seteambackend-saimadhav.rhcloud.com/send_message/");
+
+                connection = (HttpURLConnection) url.openConnection();
+
+                connection.setDoOutput(true);
+                // is output buffer writter
+                connection.setRequestMethod("POST");
+
+                Writer writer = new BufferedWriter(new OutputStreamWriter(connection.getOutputStream()));
+                writer.write(PayloadData);
+                writer.close();
+                PayloadData="";
+                int errorCode = connection.getResponseCode();
+                System.out.println("GetErrorStream " + errorCode);
+                InputStream inputStream = connection.getInputStream();
+                //input stream
+                StringBuffer buffer = new StringBuffer();
+                if (inputStream == null) {
+                    // Nothing to do.
+                    return null;
+                }
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String inputLine;
+                while ((inputLine = reader.readLine()) != null)
+                    buffer.append(inputLine + "\n");
+                if (buffer.length() == 0)
+                {
+
+                    return null;
+                }
+                JsonResponse = buffer.toString();
+//response data
+
+                Log.i("Server response", JsonResponse);
+
+                // System.out.println("response: "+JsonResponse);
+                return JsonResponse;
+
+
+
+
+
+
+            } catch (IOException e) {
+
+                e.printStackTrace();
+            }
+            finally
+            {
+                if (connection != null) {
+
+                    connection.disconnect();
+                }
+                if (reader != null) {
+                    try {
+
+                        reader.close();
+                    } catch (final IOException e) {
+
+                        Log.e(TAG, "Error closing stream", e);
+                    }
+                }
+            }
+            return null;
+        }
+
+
+        @Override
+        protected void onPostExecute(String s) {
+
+            Toast.makeText(getBaseContext(), "Data Sent!", Toast.LENGTH_LONG).show();
+
+
+        }
+
+    }
+
+
+
+
+
+}
